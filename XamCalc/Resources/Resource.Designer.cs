a6b00d04486a1f2471ce0f41840c26f5527652cb
patrickHD <patrickhdaniel@outlook.com>
2018-02-15 19:12:17 -0600

    Finished layout and logic. Added icon.

--- XamCalc/Resources/Resource.Designer.cs
+++ XamCalc/Resources/Resource.Designer.cs
@@ -231,40 +231,43 @@ namespace XamCalc
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int notification_action_background = 2130837504;
+			public const int icon = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int notification_bg = 2130837505;
+			public const int notification_action_background = 2130837505;
 			
 			// aapt resource value: 0x7f020002
-			public const int notification_bg_low = 2130837506;
+			public const int notification_bg = 2130837506;
 			
 			// aapt resource value: 0x7f020003
-			public const int notification_bg_low_normal = 2130837507;
+			public const int notification_bg_low = 2130837507;
 			
 			// aapt resource value: 0x7f020004
-			public const int notification_bg_low_pressed = 2130837508;
+			public const int notification_bg_low_normal = 2130837508;
 			
 			// aapt resource value: 0x7f020005
-			public const int notification_bg_normal = 2130837509;
+			public const int notification_bg_low_pressed = 2130837509;
 			
 			// aapt resource value: 0x7f020006
-			public const int notification_bg_normal_pressed = 2130837510;
+			public const int notification_bg_normal = 2130837510;
 			
 			// aapt resource value: 0x7f020007
-			public const int notification_icon_background = 2130837511;
-			
-			// aapt resource value: 0x7f02000a
-			public const int notification_template_icon_bg = 2130837514;
-			
-			// aapt resource value: 0x7f02000b
-			public const int notification_template_icon_low_bg = 2130837515;
+			public const int notification_bg_normal_pressed = 2130837511;
 			
 			// aapt resource value: 0x7f020008
-			public const int notification_tile_bg = 2130837512;
+			public const int notification_icon_background = 2130837512;
+			
+			// aapt resource value: 0x7f02000b
+			public const int notification_template_icon_bg = 2130837515;
+			
+			// aapt resource value: 0x7f02000c
+			public const int notification_template_icon_low_bg = 2130837516;
 			
 			// aapt resource value: 0x7f020009
-			public const int notify_panel_notification_icon_bg = 2130837513;
+			public const int notification_tile_bg = 2130837513;
+			
+			// aapt resource value: 0x7f02000a
+			public const int notify_panel_notification_icon_bg = 2130837514;
 			
 			static Drawable()
 			{
